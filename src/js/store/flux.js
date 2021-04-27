1b6621930405a23fec3ef1778a8a37a4ab9c9748
Alejandro Novellino <alnovellino07@gmail.com>
2021-04-27 13:27:41 -0400

    Api fetching implemented

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,23 +1,72 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			baseUrl: "https://www.swapi.tech/api/",
+			characters: [],
+			planets: []
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+			// Use getActions to call a function within a function
+			fetchPages: async toFind => {
+				// Fetch the pages of information
+
+				// Get the store and the actions
+				const store = getStore();
+				const actions = getActions();
+
+				try {
+					const response = await fetch(store.baseUrl + toFind);
+
+					if (response.ok) {
+						let body = await response.json();
+						let totalPages = body.total_pages;
+						let pagesCounter = 1;
+						while (pagesCounter <= totalPages) {
+							await actions.fetchElements(toFind, body.results);
+
+							// Updating the counter variable
+							pagesCounter++;
+
+							// Fetching the new page
+							if (body.next != null) {
+								const auxResponse = await fetch(body.next);
+								body = await auxResponse.json();
+							}
+						}
+					}
+				} catch (error) {
+					console.log(`🚀 ~ file: flux.js ~ line 37 ~ fetchPages: ~ error`, error);
+				}
+			},
+			fetchElements: async (toFind, elements) => {
+				// Fetch each element of the pages
+
+				// Get the store
+				const store = getStore();
+
+				// Aux to store the values fetched
+				const auxList = [];
+
+				for (const element of elements) {
+					try {
+						const response = await fetch(element.url);
+						const body = await response.json();
+
+						if (response.ok) auxList.push(body.result);
+					} catch (error) {
+						console.log(`🚀 ~ file: flux.js ~ line 44 ~ fetchElements: ~ error`, error);
+					}
+				}
+
+				if (toFind == "people/") {
+					setStore({
+						characters: [...store.characters, ...auxList]
+					});
+				} else if (toFind == "planets/") {
+					setStore({
+						planets: [...store.planets, ...auxList]
+					});
+				}
 			},
 			loadSomeData: () => {
 				/**
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,35 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext } from "react";
+import { CardGroup, Container, Row } from "react-bootstrap";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+// Components imports
+import { MyCardChar } from "../component/MyCardChar";
+
+// Importing the Context
+import { Context } from "../store/appContext";
+
+export const Home = () => {
+	// Using the context
+	const { store, actions } = useContext(Context);
+
+	return (
+		<Container fluid>
+			{/* Characters row*/}
+			<Row className="">
+				{store.characters.map(char => {
+					return (
+						<MyCardChar
+							key={char.uid}
+							name={char.properties.name}
+							gender={char.properties.gender}
+							hair_color={char.properties.hair_color}
+							eye_color={char.properties.eye_color}
+						/>
+					);
+				})}
+			</Row>
+			{/* Planets row*/}
+			<Row />
+		</Container>
+	);
+};
